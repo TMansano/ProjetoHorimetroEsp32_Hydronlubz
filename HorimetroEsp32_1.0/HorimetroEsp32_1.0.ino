9f2dcbd95e74f5d7dd66e1d0947ef9a2dd8ef89e
Thyago <thyago.mansano@uel.br>
2024-03-07 16:01:33 -0300

    versao 2.0

--- HorimetroEsp32_1.0/HorimetroEsp32_1.0.ino
+++ HorimetroEsp32_1.0/HorimetroEsp32_1.0.ino
@@ -1,11 +1,24 @@
+
+/*
+//3E UEL  Universidade Estadual de Londrina 2024
+------------------------------- Codificação de desenvolvimento de software de arduino ------------------
+horimetro contador ultra preciso com controle automatico de escala e fator de erro previsivél
+
+Este código se refere ao dispositivo desenvolvido pela 3E UEL Engenharia, na qual se refe a um contador de alta precisão que se automaticamente 
+corrige com tempo, sendo assim um contador em malha fechada com relação a sua saida [numero de horas totais e ciclos], o mesmo também conta 
+com sensor ultra rápido de tensão para aquisição dos dados de inicio e fim de contagem e também salvamento dos dados em um cartão de 
+memória micro SD, o código é sequencial entretante após melhorias o código funcina com base em dois núcloeos do processodr presente 
+na ESP32, a programação multicore foi necessaria para aumentar a velocidade de processamento do contador e melhorar sua precisão.
+
+as bibliotecas necessaria estão abaixo com observação a biblioteca LiquidCrystal_I2C.h que deve ser instalada separadamente na maquina assim como a placa ESP32 Dev Module da ESP 32 de 36 pinos.
+*/
+
 #include <Arduino.h>
 #include <LiquidCrystal.h>  // Biblioteca LCD
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
-#include "FS.h"
-#include <SPIFFS.h>
 
 #define CS 5  //Chip Select SD Card.
 #define LEDPIN 25
@@ -13,7 +26,6 @@
 #define BUTTON2 26
 #define ANALOGREADING 33
 #define BUZZERPIN 17
-
 //----------------------Declaração de variáveis globais-------------------------
 int sec = 0;
 int minute = 0;
@@ -22,146 +34,99 @@ unsigned long ciclos = 0;
 int bstop = 0;
 int i = 0;
 unsigned long next_millis = 0;
+int erro_medio = 0;
 //----------------------char strings-----------------------------------
-char buf[21];
-char buf2[23];
+char buf[25];
+char buf2[25];
+char buf3[28];
 char hr[12];
 char minutes[10];
 char secs[10];
 char ciclos2[12];
+//char arquivo_maior[15];  //define o char onde sera o arquivo não corrompido ou intacto
 //char dados[]= "/dados.h"; //loca onde será salvo os dados na memoria SD
-
 //variaveis do voltimetro--------------------------
 int amostragem = 600;
 float VResistor = 0;
 float VPico = 0;
 float valorFinal = 0;
-
 //função de reboot do microcontrolador
 void (*funcReset)() = 0;
-
 //define o arquivo a ser salvo os dados
 File variaveis;
-
 //define a inicialização do LCD i2c
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+float millis_voltimetrofinal=0;
+//variaveis que indicam o núcleo
+static uint8_t taskCoreZero = 0;
+float millis_voltimetroinicial =0;
+
 
 void setup() {
-
   //-------------definição da inicialização dos sistemas--------------------------
-  lcd.begin(16, 2);
-  lcd.init();
-  lcd.backlight();
   Serial.begin(9600);
   //-------------Entradas e Saidas----------------------------------------
   pinMode(17, OUTPUT);        //TBJ for Buzzer
   pinMode(25, OUTPUT);        // Reset LED
   pinMode(27, INPUT_PULLUP);  //Reset BUTTON1
   pinMode(26, INPUT_PULLUP);  //Calibration BUTTON2
-  pinMode(33, INPUT);         //Input Voltage Sensor.
-  delay(200);
+  pinMode(33, INPUT_PULLDOWN);         //Input Voltage Sensor.
+  delay(10);                 //delay for stabilize pins
+   //cria uma tarefa que será executada na função coreTaskTwo, com prioridade 2 e execução no núcleo 0
+   //coreTaskTwo: vigiar o botão para detectar quando pressioná-lo
+   xTaskCreatePinnedToCore(
+                    coreTaskTwo,   /* função que implementa a tarefa */
+                    "coreTaskTwo", /* nome da tarefa */
+                    10000,      /* número de palavras a serem alocadas para uso com a pilha da tarefa */
+                    NULL,       /* parâmetro de entrada para a tarefa (pode ser NULL) */
+                    0,          /* prioridade da tarefa (0 a N) */
+                    NULL,       /* referência para a tarefa (pode ser NULL) */
+                    taskCoreZero);         /* Núcleo que executará a tarefa */
+
+        delay(500); //tempo para a tarefa iniciar
+
+  
+  //------------------------leitura de tensão inicial---------------------
+  Voltimetro();
   //inicialização do cartão---------------------------------------------
   if (!SD.begin(5)) {
+    delay(3500);
     lcd.clear();
     lcd.print("Falha no SD Card");
-    //Serial.println("Falha no SD Card.");
-    while (1)
-      ;
+    while (!SD.begin(5));
+    lcd.clear();
   }
   //------------------------Bruteforce Reset System---------------------
   if (digitalRead(BUTTON1) == LOW) {
-    //SD.remove("/dados3.txt");
     reset();  //chama o reset e eresta o horimetro
   }
-
-
-
-
-
   if (!SD.exists("/dados3.txt") && !SD.exists("/dados4.txt")) {
 
-    sprintf(buf2, "%05d:%02d:%02d:%08d", hrs, minute, sec, ciclos);
-    WriteFile("/dados3.txt", buf2);
-    WriteFile("/dados4.txt", buf2);
-
-
-  } else {
-    
-    variaveis = SD.open("/dados3.txt");
-
-    int tamanho1=variaveis.size();
-    
-
-
-
-
-    if (variaveis) {
-
-      variaveis.seek(variaveis.size() - 22);  //aponta pra a ultima linha (ultimo salvamento dos dados)
-
-      //4 fors: faz a varedura da linha buscando as variaveis salvas do horimetro
-      for (i = 0; i < 5; i++) {
-        hr[i] = variaveis.read();
-      }
-      variaveis.read();  //pula o ":"
-      i = 0;
-      for (i = 0; i < 2; i++) {
-        minutes[i] = variaveis.read();
-      }
-      variaveis.read();  //pula o ":"
-      i = 0;
-      for (i = 0; i < 2; i++) {
-        secs[i] = variaveis.read();
-      }
-      variaveis.read();  //pula o ":"
-      i = 0;
-      for (i = 0; i < 8; i++) {
-        ciclos2[i] = variaveis.read();
-      }
-      variaveis.close();  //fecha o arquivo apos a leitura
-    }
-
-
-    //faz a conversão para Inteiros e coloca na memoria de trabalho do esp
-    sec = std::__cxx11::stoi(secs);
-    minute = std::__cxx11::stoi(minutes);
-    hrs = std::__cxx11::stoi(hr);
-    ciclos = std::__cxx11::stoi(ciclos2);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  if (!SD.exists("/dados3.txt")) {
     variaveis = SD.open("/dados3.txt", FILE_WRITE);
     if (variaveis) {
-      //Serial.println("Arquivo não exites aqui");
-      //delay(3000);
       sprintf(buf2, "%05d:%02d:%02d:%08d", hrs, minute, sec, ciclos);
       WriteFile("/dados3.txt", buf2);
     }
+    variaveis = SD.open("/dados4.txt", FILE_WRITE);
+    if (variaveis) {
+      WriteFile("/dados4.txt", buf2);
+    }
   } else {
+    
+
     variaveis = SD.open("/dados3.txt");
+    int A = variaveis.size();
+    variaveis.close();
+    delay(5);
+
+    variaveis = SD.open("/dados4.txt");
+    int B = variaveis.size();
+    variaveis.close();
+    delay(5);
+    
+    if (A >= B) {variaveis = SD.open("/dados3.txt");} 
+    else {variaveis = SD.open("/dados4.txt");}
+
     if (variaveis) {
 
       variaveis.seek(variaveis.size() - 22);  //aponta pra a ultima linha (ultimo salvamento dos dados)
@@ -186,35 +151,43 @@ void setup() {
         ciclos2[i] = variaveis.read();
       }
       variaveis.close();  //fecha o arquivo apos a leitura
+      delay(10);
     }
-
-
-    Serial.println("testando leitura hr");
-    Serial.println(hr);
-    Serial.println("testando leitura minutes");
-    Serial.println(minutes);
-    Serial.println("testando leitura secs");
-    Serial.println(secs);
-    Serial.println("testando leitura ciclos");
-    Serial.println(ciclos2);
-
-
     //faz a conversão para Inteiros e coloca na memoria de trabalho do esp
     sec = std::__cxx11::stoi(secs);
     minute = std::__cxx11::stoi(minutes);
     hrs = std::__cxx11::stoi(hr);
     ciclos = std::__cxx11::stoi(ciclos2);
   }
-
   //maquina ligou, adiciona um ciclo
   ciclos++;
+  //fator de correção de escala (tempo total errado em s) é de: +-(0.008xTtotal) + (0.5xCiclos).
+  erro_medio = (ciclos % 2);
+  sec += erro_medio;
+ 
+//------------------------------------Data Logger (registro de informações a longo prazo----------------------------------------------
+  if (!SD.exists("/dados5.txt")) {
+    variaveis = SD.open("/dados5.txt", FILE_WRITE);
+    if (variaveis) {
+      WriteFile("/dados5.txt", "Horas:Minutos:Segundos:Ciclos:Tensão");
+      sprintf(buf3, "%05d:%02d:%02d:%08d:%.1f", hrs, minute, sec, ciclos,valorFinal);
+      WriteFile("/dados5.txt", buf3);
+    }
+  } else {
+    sprintf(buf3, "%05d:%02d:%02d:%08d:%.1f", hrs, minute, sec, ciclos, valorFinal);
+    WriteFile("/dados5.txt", buf3);
+    }
+//--------------------------- Algoritmo de escrita no cartão de memória ---------------------------
+    sprintf(buf2, "%05d:%02d:%02d:%08d", hrs, minute, sec, ciclos);  //string a ser salva na memória cartão SD
+
+    WriteFile("/dados3.txt", buf2);  //fução que salva os dados no SD
+    delay(5); //(delay de estabilização)
+    WriteFile("/dados4.txt", buf2);
 }
 
 void loop() {
-
   //chama a função voltimetro e analisa o nivel de tensão
   Voltimetro();
-
   if (digitalRead(BUTTON1) == LOW) {
     reset();  //chama o reset e eresta o horimetro
   }
@@ -240,36 +213,30 @@ void loop() {
     bstop++;
   }
 
-
-
-
-
   //----------------------------Contador-------------------------------------
-  //contador oscilador auto calibrante, "calibra" com o tempo, contador preciso. e que só aconta apenas e apenas se valorFinal > 80 Volts.
+  //contador oscilador auto calibrante, "calibra" com o tempo, contador preciso.
 
-  if (((millis() / 1000) >= next_millis) && (valorFinal > 80)) {
+  if (((millis() / 1000) >= next_millis)) {
     next_millis += 1;  //registrador de clock.
-
     sec++;  // conta um segundo!.
-
     minute += (sec / 60);
     hrs += minute / 60;
     minute %= 60;
     sec = sec % 60;
 
-    sprintf(buf2, "%05d:%02d:%02d:%08d", hrs, minute, sec, ciclos);  //string a ser salva na memória cartão SD
-    sprintf(buf, "%05d:%02d:%02d     ", hrs, minute, sec);           //string a ser impressa na LCD
-
+    //--------------------------- Algoritmo de escrita no LCD ---------------------------
+    sprintf(buf, "%05d:%02d:%02d     ", hrs, minute, sec);  //string a ser impressa na LCD
     lcd.print("Hora-maquina");
     lcd.setCursor(0, 1);
     lcd.println(buf);
+    //--------------------------- Algoritmo de escrita no cartão de memória ---------------------------
+    sprintf(buf2, "%05d:%02d:%02d:%08d", hrs, minute, sec, ciclos);  //string a ser salva na memória cartão SD
 
     WriteFile("/dados3.txt", buf2);  //fução que salva os dados no SD
+    delay(10); //(delay de estabilização)
+    WriteFile("/dados4.txt", buf2);
   }
 }
-
-
-
 
 void WriteFile(const char* path, const char* message) {
   // open the file. note that only one file can be open at a time,
@@ -277,33 +244,26 @@ void WriteFile(const char* path, const char* message) {
   variaveis = SD.open(path, FILE_WRITE);
   // if the file opened okay, write to it:
   if (variaveis) {
-    Serial.println("Objeto Aberto.");
-    Serial.println("Writing to %s ", path);
-    delay(5000);
-
     //aponta para o final e salva a string contendo os dados atualizados do horimetro, assim, acumulando os dados no aquivo h
     variaveis.seek(variaveis.size());
     variaveis.println(message);
     variaveis.close();  // close the file:
-
-    delay(5000);
-    Serial.println("completo, objeto Fechado..");
-    //Serial.println("Objeto Fechado.");
-    //lcd.noBacklight();
-    //delay(5000);
-
+    delay(10);
   }
   // if the file didn't open, print an error:
   else {
+    lcd.clear();
+    lcd.println("Erro ao Abrir o ");
+    lcd.setCursor(0, 1);
+    lcd.println("Cartao SD.      ");
     Serial.println("Erro ao Abrir o Arquivo");
     Serial.println(path);
+    
   }
 }
 
 //------------------Função voltimetro------------------
 void Voltimetro() {
-
-
   for (int i = 0; i < amostragem; i++) {
     //Leitura do valor na porta analógica (varia de 0 a 4096)
     if (analogRead(ANALOGREADING) > VPico) {
@@ -324,6 +284,9 @@ void reset() {
   Serial.print("Horimetro Redefinido");
   lcd.setCursor(0, 1);
   lcd.print("Horimetro...");
+  delay(1000);
+  lcd.clear();
+  lcd.print("Aguarde");
   delay(2000);
   digitalWrite(LEDPIN, LOW);
   delay(100);
@@ -344,10 +307,12 @@ void reset() {
   hrs = 0;
   ciclos = 0;
   SD.remove("/dados3.txt");
+  delay(20);
+  SD.remove("/dados4.txt");
+  delay(20);
+  SD.remove("/dados5.txt");
+  delay(20);
   funcReset();
-
-  //volta ao "normal/operação" função principal
-  loop();
 }
 
 //-------------------Função Calibragem-------------------
@@ -400,21 +365,31 @@ void mostra_valores() {
 
 
 
-/*
-
-    Serial.println("Teste de pre-falha"); //linha utilizada para debug e teste da execução do código
-    delay(25000);
-
-    */
 
 
-//medir tempo com millis
-//float millis_voltimetroinicial = millis();
-//float millis_voltimetrofinal = millis();
-// float time_diference = (millis_voltimetrofinal - millis_voltimetroinicial);
-//Serial.println("Tempo de execução da função voltimetro");
-//Serial.println(time_diference);
-//delay(5000);
+
+//essa função será responsável por ler o estado do botão
+//e atualizar a variavel de controle.
+void coreTaskTwo( void * pvParameters ){
+
+//--------------Inicializa o LCD------------------------
+  lcd.begin(16,2);
+  lcd.init();
+  // Liga a luz do display
+  lcd.backlight();
+
+     while(true){
+      delay(20);
+    } 
+}
+
+
+
+
+
+
+
+
 /*
 //------------------------Função utilizada para leitura na comunicação serial do esp com computador, lê o Cartão SD
 void ReadFile(const char* path) {
